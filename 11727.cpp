19a042cec01d0dfb7d31a3e882b8c666cf803ea8
Yujeongju <36145118+Yujeongju@users.noreply.github.com>
2019-05-26 15:53:28 +0900

    2019년 5월 25일(토) 업데이트
    간단한 DP 문제 두 개 해결

---
+++ 11727.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main(void)
+{
+	int num, dp[1010];
+	scanf("%d", &num);
+	
+	dp[0] = 1;
+	dp[1] = 3;
+	for(int i=2; i<num; i++){
+		dp[i] = (dp[i-1]+(dp[i-2]*2))%10007;
+	}
+
+	printf("%d", dp[num-1]);
+
+}
+
